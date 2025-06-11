c079ee7456cb0c80928e1639b2bc20a36f056c8a
John Doe <naryasomayaj@umass.edu>
2025-06-11 12:08:59 -0400

    Update DatabaseConnection test file name

--- tests/DatabaseConnectionTest.py
+++ tests/DatabaseConnectionTest.py
@@ -1,11 +1,11 @@
 import pytest 
 from analytics.DatabaseConnection import DatabaseConnection
 import pandas as pd
-def createDBConnection():
-    return DatabaseConnection("C:/Users/Nitya Karthik A/ds4cg-job-analytics/data/slurm_data_small.db")
+def createDBConnection(path: str = "C:/Users/Nitya Karthik A/ds4cg-job-analytics/data/slurm_data_small.db") -> DatabaseConnection:
+    return DatabaseConnection(path)
 
-def test_db_connection():
-    db_conn = createDBConnection()
+def test_db_connection(path: str = "C:/Users/Nitya Karthik A/ds4cg-job-analytics/data/slurm_data_small.db"):
+    db_conn = createDBConnection(path)
     assert db_conn.is_connected() == True
     assert isinstance(db_conn.as_dataframe(), pd.DataFrame)
     db_conn.disconnect()
