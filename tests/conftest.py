16fe2141d85c5bcde8c2d2633128f416d81abf1e
Tan Le <letan101104@gmail.com>
2025-07-02 10:04:51 -0400

    fix test_database : add proper try catch to handle database issue in conftest.py and convert_csv_to_db.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,12 +8,15 @@ import shutil
 @pytest.fixture(scope="module")
 def mock_data_frame():
     temp_db_dir = tempfile.mkdtemp()
+    mem_db = None
     try:
         temp_db_path = f"{temp_db_dir}/mock.db"
         convert_csv_to_db("tests/mock_data/mock.csv", temp_db_path)
-        db = DatabaseConnection(temp_db_path)
-        yield db.fetch_all_jobs()
+        mem_db = DatabaseConnection(temp_db_path)
+        yield mem_db.fetch_all_jobs()
     except Exception as e:
         raise Exception("Exception at mock_data_frame") from e
     finally:
+        if mem_db is not None:
+            mem_db.disconnect()
         shutil.rmtree(temp_db_dir)
