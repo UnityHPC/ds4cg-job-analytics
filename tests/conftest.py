2f6f85cad7b44d50797da16427b0f251e47c14bc
Espiobest <59823894+Espiobest@users.noreply.github.com>
2025-08-07 14:44:31 -0400

    remove redundant type hint for generator

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ from .mock_data.convert_csv_to_db import convert_csv_to_db
 
 
 @pytest.fixture(scope="module")
-def mock_data_frame(request: pytest.FixtureRequest) -> Generator[pd.DataFrame, None, None]:
+def mock_data_frame(request: pytest.FixtureRequest) -> Generator[pd.DataFrame]:
     temp_db_dir = tempfile.mkdtemp()
     mem_db = None
     try:
